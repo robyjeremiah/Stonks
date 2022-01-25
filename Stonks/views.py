c08aa22fafda21970ba655dbf98d346f1b7c263a
Jalen Springer <dictionary_3@hotmai.com>
2022-01-25 01:19:51 -0500

    first commit

---
+++ Stonks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Welcome to the Home Page!')
+
+def profile(request):
+    return HttpResponse('Welcome to the Profile Page!')
+
+def test(request):
+    return HttpResponse('Welcome to the Test Page!')
