666c525e087a31101d558bcf8e0b778193b38a32
Samir Spraggins <sspragg2@students.kennesaw.edu>
2022-02-04 22:52:38 -0500

    Updated the settings for Database

--- Stksite/settings.py
+++ Stksite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+#database operating system variable
+OS = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +87,7 @@ if 'RDS_HOSTNAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-elif 'HOST' == 'Stonks_mysql':
+elif (OS == True):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
