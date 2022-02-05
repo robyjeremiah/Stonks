557a252a81aa6c565fadf70d432fb08cd2d8d81a
Jalen Springer <dictionary_3@hotmai.com>
2022-02-04 19:39:35 -0500

    Made the login in page the homescreen

--- Stonks/views.py
+++ Stonks/views.py
@@ -4,15 +4,6 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the Home Page!')
-
-def profile(request):
-    return HttpResponse('Welcome to the Profile Page! Bubble butt')
-
-def test(request):
-    return HttpResponse('Welcome to the Test Page!')
-
-def login(request):
     return render(request, 'login.html')
 
 def newUser(request):
