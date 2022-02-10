a7fc5aeeb8011b7a086d943f90be542a94e08b87
robyjeremiah <46535871+robyjeremiah@users.noreply.github.com>
2022-02-10 17:45:21 -0500

    Login/Registraion works with the extraness that he wanted

--- Stonks/forms.py
+++ Stonks/forms.py
@@ -1,3 +1,4 @@
+from urllib.request import DataHandler
 from django import forms
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -9,9 +10,26 @@ class CustomUserForm(UserCreationForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    dob = forms.DateField()
     #password1 = forms.CharField()
     #password2 = forms.CharField()
     
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','email', 'password1' ,'password2' )
+        fields = ('email','first_name','last_name','dob','password1' ,'password2')
+        
+    def save(self):
+        data = self.cleaned_data
+        user = User(email=data['email'], 
+                    first_name=data['first_name'],
+                    last_name=data['last_name'],
+                    dob= data['dob'],)
+        if(str(data['password1']) == str(data['password2'])):
+            user.set_password(data['password1'])
+        tempyear= str(user.dob.year)[2] + str(user.dob.year)[3]
+        if user.dob.month >=10:
+            user.username = str(user.first_name[0])+str(user.last_name)+str(user.dob.month)+tempyear
+        else:
+            user.username = str(user.first_name[0])+str(user.last_name)+"0"+str(user.dob.month)+tempyear
+        #user.set_password(user.password1)
+        user.save()
--- Stonks/models.py
+++ Stonks/models.py
@@ -8,48 +8,59 @@ from django.utils.translation import gettext_lazy as _
 class UserManager(BaseUserManager):
     #use_in_migrations = True
 
-    def _create_user(self, username, password, **extra_fields):
-        if not username:
-            raise ValueError('The given username must be set')
+    def _create_user(self, email, password, dob, first_name, last_name,**extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
         #email = self.normalize_email(email)
-        user = self.model(username = username, **extra_fields)
+        user = self.model(email = self.normalize_email(email), 
+                          dob = dob,
+                          first_name = first_name,
+                          last_name = last_name,
+                          **extra_fields)     
+        tempyear= str(dob.year)[2] + str(dob.year)[3]
+        if dob.month >=10:
+            user.username = str(first_name[0])+str(last_name)+str(dob.month)+tempyear
+        else:
+            user.username = str(first_name[0])+str(last_name)+"0"+str(dob.month)+tempyear
         user.set_password(password)
         user.save(using=self._db)
+        print(user.username)
+        print(user.dob)
         return user
 
-    def create_user(self, username, password, **extra_fields):
+    def create_user(self, email, password, dob, first_name, last_name,**extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, password, **extra_fields)
+        return self._create_user(email, password, dob, first_name, last_name,**extra_fields)
 
-    def create_superuser(self, username, password, **extra_fields):
+    def create_superuser(self, email, password, dob, first_name, last_name,**extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(username, password, **extra_fields)
+        return self._create_user(email, password, dob, first_name, last_name,**extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_('username'), max_length=30, blank=True, unique=True)
+    username = models.CharField(_('username'), max_length=30, blank=True)
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
-    is_active = models.BooleanField(_('active'), default=True)
     address = models.CharField(_('address'), max_length=254, blank=True)
     dob = models.DateField(_('DateofBirth'),auto_now = False,blank = True, null = True)
     #avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    is_active = models.BooleanField(_('active'), default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name','dob']
 
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
+    # class Meta:
+    #     verbose_name = _('user')
+    #     verbose_name_plural = _('users')
