8ae4c1bddf8e712727904a71033e5cdf90f19e78
Jalen Springer <dictionary_3@hotmai.com>
2022-02-20 12:16:59 -0500

    Implemented logic for manager

--- Stonks/decorators.py
+++ Stonks/decorators.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwa):
         if request.user.is_authenticated and request.user.groups.filter(name='Administrator'):
             return redirect('/adminHome/')
         elif request.user.is_authenticated and request.user.groups.filter(name='Accountant'):
             return redirect('/generalHome/')
+        elif request.user.is_authenticated and request.user.groups.filter(name="Manager"):
+            return redirect('/adminHome/')
         else:
             return view_func(request, *args, **kwa)
     return wrapper_func
+
 
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
