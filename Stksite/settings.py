fe28c8b181484c3c4e99e47523425a6cab5d1e3f
robyjeremiah <46535871+robyjeremiah@users.noreply.github.com>
2022-02-26 18:27:46 -0500

    fixing merge

--- Stksite/settings.py
+++ Stksite/settings.py
@@ -75,42 +75,35 @@ WSGI_APPLICATION = 'Stksite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+elif(env('DB_ENV') == 'SQLite'):
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-# if 'RDS_HOSTNAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ['RDS_PORT'],
-#         }
-#     }
-# elif(env('DB_ENV') == 'SQLite'):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': env('DB_NAME'),
-#             'USER': env('MY_USER'),
-#             'PASSWORD': env('MY_PASSWORD'),
-#             'HOST': env('DB_ENV'),
-#             'PORT': 3306
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': env('DB_NAME'),
+            'USER': env('MY_USER'),
+            'PASSWORD': env('MY_PASSWORD'),
+            'HOST': env('DB_ENV'),
+            'PORT': 3306
+        }
+    }
 
 AUTH_USER_MODEL = 'Stonks.User'
 
