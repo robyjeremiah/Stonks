bdf3111d1d6128e772178b865eba2d9c4a43a8e5
Jalen Springer <dictionary_3@hotmai.com>
2022-02-27 18:10:50 -0500

    Stuff for the later

--- Stonks/admin.py
+++ Stonks/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.User)
+admin.site.register(models.Account)
 admin.site.register(models.SecurityQuestion)
 admin.site.register(models.SecurityAnswer)
