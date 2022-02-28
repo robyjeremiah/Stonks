1868d980a664e8e3dd5266f3f15c228b67e3e745
Liv <ltayl150@students.kennesaw.edu>
2022-02-27 19:51:37 -0500

    EventLog view added, cannot find evenlog button.

--- Stonks/urls.py
+++ Stonks/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
         template_name='passwordReset.html'), name="password_reset_confirm"),
     # path ('passwordConfirm/', auth_views.PasswordResetCompleteView.as_view(template_name='passwordConfirm.html'), name="password_reset_complete"),
     path('chartOfAccounts/viewAccountInfo/',
-         views.useraccount, name='viewAccount')
+         views.useraccount, name='viewAccount'),
+    path('chartOfAccounts/viewEventLog/', views.eventlog, name='EventLog')
 ]
